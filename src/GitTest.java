eb091b634160adec812fc9ca3889e016e5e28d2b
Mark Frymire <mark.frymire@dac.us>
2014-05-20 09:16:48 -0400

    Life is ruined.

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,8 +6,9 @@ public class GitTest {
 
 		System.out.println("Hello, world.");
 		System.out.println("Lovin' life...");
+		System.out.println("You've ruined my life!!!");
 		System.out.println("Goodbye, cruel world.");
 
 	} // main
 
-}
+} // GitTest
