e78b0aa8b015ed16f57a2b3c74f763bb0e66c189
Mark Frymire <mark.frymire@dac.us>
2014-05-20 08:57:01 -0400

    Add a comment at the end of the main method.

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,8 +4,9 @@ public class GitTest {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, world.");
+		System.out.println("Lovin' life...");
 		System.out.println("Goodbye, cruel world.");
 
-	}
+	} // main
 
 }
