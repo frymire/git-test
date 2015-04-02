16ad50950022260cd7216b0898a5c6f9ef709b56
Mark Frymire <mark.frymire@dac.us>
2015-04-02 11:12:22 -0400

    Add GitTest2 class

---
+++ src/GitTest2.java
@@ -0,0 +1,13 @@
+
+// Just a test so that there will be more than one file in the repository.
+public class GitTest2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, world.");
+		System.out.println("Lovin' life...");
+		System.out.println("Goodbye, cruel world.");
+
+	} // main
+
+} // GitTest2
